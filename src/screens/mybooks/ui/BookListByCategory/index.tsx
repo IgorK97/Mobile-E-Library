10b450471c25e7d40564e085c6bc2305dc8602b3
IgorK97 <kt.wert.74@gmail.com>
2025-11-26 21:42:02 +0300

    Started adding book shelves

---
+++ src/screens/mybooks/ui/BookListByCategory/index.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+interface BookListByCategoryProps {
+  CategoryId: number;
+}
+
+export const BookListByCategory = ({ CategoryId }: BookListByCategoryProps) => {
+  return <View></View>;
+};
--- src/screens/mybooks/ui/mybooksComponent/index.tsx
+++ src/screens/mybooks/ui/mybooksComponent/index.tsx
@@ -19,150 +19,30 @@ import { commonStyles } from "@/src/shared/lib/constants/common";
 import { Colors } from "@/src/shared/lib/constants/theme";
 import { useColorScheme } from "@/src/shared/lib/hooks/use-color-scheme";
 import { useStore } from "@/src/shared/lib/store/globalStore";
-// const books: BookListItem[] = [
-//   {
-//     id: 1,
-//     title: "Буддизм в Японии",
-//     // author: "Т.П. Григорьева",
-//     rating: 4.5,
-//     reviewCount: 10,
-//     pages: 704,
-//     year: 1993,
-//     description:
-//       "Монография является первой в отечественной литературе попыткой проследить пути становления японского буддизма и его влияние на культуру Японии.",
-//     imageUrl: require("@assets/images/book_1.png"),
-//     genres: [
-//       "Философия",
-//       "Культурология",
-//       "Религия",
-//       "Буддизм",
-//       "Восток",
-//       "Япония",
-//     ],
-//     imageBase64: "",
-//   },
-//   {
-//     id: 2,
-//     title: "Буддизм в Японии",
-//     author: "Т.П. Григорьева",
-//     rating: 4.5,
-//     reviewCount: 10,
-//     pages: 704,
-//     year: 1993,
-//     description:
-//       "Монография является первой в отечественной литературе попыткой проследить пути становления японского буддизма и его влияние на культуру Японии.",
-//     imageUrl: require("@assets/images/book_1.png"),
-//     genres: [
-//       "Философия",
-//       "Культурология",
-//       "Религия",
-//       "Буддизм",
-//       "Восток",
-//       "Япония",
-//     ],
-//     imageBase64: "",
-//   },
-//   {
-//     id: 3,
-//     title: "Буддизм в Японии",
-//     author: "Т.П. Григорьева",
-//     rating: 4.5,
-//     reviewCount: 10,
-//     pages: 704,
-//     year: 1993,
-//     description:
-//       "Монография является первой в отечественной литературе попыткой проследить пути становления японского буддизма и его влияние на культуру Японии.",
-//     imageUrl: require("@assets/images/book_1.png"),
-//     genres: [
-//       "Философия",
-//       "Культурология",
-//       "Религия",
-//       "Буддизм",
-//       "Восток",
-//       "Япония",
-//     ],
-//     imageBase64: "",
-//   },
-//   {
-//     id: 4,
-//     title: "Буддизм в Японии",
-//     author: "Т.П. Григорьева",
-//     rating: 4.5,
-//     reviewCount: 10,
-//     pages: 704,
-//     year: 1993,
-//     description:
-//       "Монография является первой в отечественной литературе попыткой проследить пути становления японского буддизма и его влияние на культуру Японии.",
-//     imageUrl: require("@assets/images/book_1.png"),
-//     genres: [
-//       "Философия",
-//       "Культурология",
-//       "Религия",
-//       "Буддизм",
-//       "Восток",
-//       "Япония",
-//     ],
-//     imageBase64: "",
-//   },
-//   {
-//     id: 5,
-//     title: "Буддизм в Японии",
-//     author: "Т.П. Григорьева",
-//     rating: 4.5,
-//     reviewCount: 10,
-//     pages: 704,
-//     year: 1993,
-//     description:
-//       "Монография является первой в отечественной литературе попыткой проследить пути становления японского буддизма и его влияние на культуру Японии.",
-//     imageUrl: require("@assets/images/book_1.png"),
-//     genres: [
-//       "Философия",
-//       "Культурология",
-//       "Религия",
-//       "Буддизм",
-//       "Восток",
-//       "Япония",
-//     ],
-//     imageBase64: "",
-//   },
-//   {
-//     id: 6,
-//     title: "Буддизм в Японии",
-//     author: "Т.П. Григорьева",
-//     rating: 4.5,
-//     reviewCount: 10,
-//     pages: 704,
-//     year: 1993,
-//     description:
-//       "Монография является первой в отечественной литературе попыткой проследить пути становления японского буддизма и его влияние на культуру Японии.",
-//     imageUrl: require("@assets/images/book_1.png"),
-//     genres: [
-//       "Философия",
-//       "Культурология",
-//       "Религия",
-//       "Буддизм",
-//       "Восток",
-//       "Япония",
-//     ],
-//     imageBase64: "",
-//   },
-// ];
+import { BookListByCategory } from "../BookListByCategory";
 
 interface MyBooksProps {
   onNavigateToBook: (id: number) => void;
 }
 
 export const MyBooks = ({ onNavigateToBook }: MyBooksProps) => {
-  const [activaTab, setActiveTab] = useState("favorites");
+  const tabs = [
+    { id: 1, label: "Избранное" },
+    { id: 2, label: "Загруженные" },
+    { id: -1, label: "История чтения" },
+    { id: 0, label: "Прочитанные" },
+  ];
+
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
   const { width } = useWindowDimensions();
   const numColumns = width < 500 ? 2 : 4;
   const cardWidth = width / numColumns - 24;
-  const tabs = [
-    { id: "favorites", label: "Избранное" },
-    { id: "downloaded", label: "Загруженные" },
-    { id: "reading", label: "История чтения" },
-    { id: "read", label: "Прочитанные" },
-  ];
+  // const tabs = [
+  //   { id: "favorites", label: "Избранное" },
+  //   { id: "downloaded", label: "Загруженные" },
+  //   { id: "reading", label: "История чтения" },
+  //   { id: "read", label: "Прочитанные" },
+  // ];
   const color = useColorScheme();
   const styles = useMyBooksStyles();
   const typography = useTypography();
@@ -202,14 +82,14 @@ export const MyBooks = ({ onNavigateToBook }: MyBooksProps) => {
             key={tab.id}
             style={[
               styles.tabButton,
-              activaTab === tab.id && styles.tabButtonSelected,
+              activeTab === tab.id && styles.tabButtonSelected,
             ]}
             onPress={() => setActiveTab(tab.id)}
           >
             <Text
               style={[
                 typography.tabText,
-                activaTab === tab.id && typography.tabTextSelected,
+                activeTab === tab.id && typography.tabTextSelected,
               ]}
             >
               {tab.label}
@@ -217,30 +97,8 @@ export const MyBooks = ({ onNavigateToBook }: MyBooksProps) => {
           </TouchableOpacity>
         ))}
       </ScrollView>
-
-      {/* <FlatList
-        data={books}
-        keyExtractor={(item) => item.id.toString()}
-        numColumns={numColumns}
-        contentContainerStyle={commonStyles.grid}
-        columnWrapperStyle={commonStyles.gridRow}
-        style={{ gap: 5, marginBottom: 5 }}
-        renderItem={({ item }) => (
-          <View style={{ width: cardWidth }}>
-            <BookCard
-              bookInfo={item}
-              key={item.id}
-              onPress={() =>
-                // router.push({ pathname: "/[id]", params: { id: item.id } })
-                {
-                  setCurrentBook(item);
-                  onNavigateToBook(item.id);
-                }
-              }
-            />
-          </View>
-        )}
-      /> */}
+      <BookListByCategory CategoryId={activeTab} />
+      {/* Enter Component Here*/}
     </View>
   );
 };
